f9a457153e6ee44aef3532833e6b8492e3fe8336
katkamrahul <katkam.rahul@gmail.com>
2022-08-29 17:54:53 +0530

    new programs

--- src/stringsExamples/P039ReadAStringAndReturnAfterRemovingASpecifiedCharAndItsImmediateLeftAndRightChars.java
+++ src/stringsExamples/P039ReadAStringAndReturnAfterRemovingASpecifiedCharAndItsImmediateLeftAndRightChars.java
@@ -1,33 +1,32 @@
 //Read a string and returns after removing a specified character & its immediate left and right characters.
 //Given character = #
-//String 1 = test#string		String 2 = test##string		String 3 = test#the#string
-//New string = testring			New string = testring		New string = teshtring	
+//String 1 = test#string	String 2 = test##string		String 3 = test#the#string	 String 4 = teststring
+//New string = testring		New string = testring		New string = teshtring		 New string = teststring
 
 package stringsExamples;
 
 public class P039ReadAStringAndReturnAfterRemovingASpecifiedCharAndItsImmediateLeftAndRightChars {
 	public static void main(String[] args) {
 		char ch = '#';
-		String[] arr = {"photo", "oocyte", "boat", "own"};
+		String s = Character.toString(ch);
+		System.out.println("Given character = " +ch);
+		String[] arr = {"test#string", "test##string", "test#the#string", "teststring"};
 		int count = 1;
 		for(int i=0; i<arr.length; i++) {
 			String str = arr[i];
 			System.out.println("String " +count+ " = " +str);
-			if(str.charAt(0)!=ch && str.charAt(1)!=ch) {
-				System.out.println("New String = " +str);
-				count++;
-			}
-			else if(str.charAt(0)==ch && str.charAt(1)!=ch) {
-				System.out.println("New String = " +str.substring(1, str.length()));
-				count++;
-			}
-			else if(str.charAt(0)!=ch && str.charAt(1)==ch) {
-				System.out.println("New String = " +(str.substring(0, 1) + str.substring(2, str.length())));
-				count++;
+			if(str.contains(s)) {
+				String s1 = "";
+				for(int j=0; j<str.length(); j++) {
+					int n = 0;
+					if(str.charAt(j)==ch) {
+						n = str.indexOf(str.charAt(j));
+						s1 = str.substring(0, n-1) + str.substring(n+1, str.length());
+					}
+				}
 			}
 			else {
-				System.out.println("New String = " +str.substring(2, str.length()));
-				count++;
+				System.out.println("Given character " +ch+ " is not present in the string");
 			}
 		}
 	}
